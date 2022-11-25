2d3f1d1d43b43a6baf78d810f30da103c053113d
Praise Idowu <ifeoluwapraise02@gmail.com>
2022-11-25 13:09:12 -0600

    Change password urls

--- account/urls.py
+++ account/urls.py
@@ -8,5 +8,12 @@ urlpatterns = [
     #Login/Logout urls
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    
+    # Change password urls
+    path('password-change/', 
+        auth_views.PasswordChangeView.as_view(), name='password_change'),  #handle d form to change d password
+    path('password-change/done', 
+        auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),  #display a success message
+    
     path('', views.dashboard, name='dashboard'),
 ]
