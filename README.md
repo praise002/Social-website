64cc47e56a337e6ac31947a4852a8e3d0fb691ec
Praise Idowu <ifeoluwapraise02@gmail.com>
2022-12-01 10:06:48 -0600

    Edit profile and messages framework

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Basic commands
 - pipenv install Django~=4.1.0
 - python manage.py createsuperuser
+- pipenv install Pillow==9.2.0
--- account/models.py
+++ account/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
 
 
+class Profile(models.Model):
+    class Gender(models.TextChoices):
+        MALE = 'M', 'Male'
+        FEMALE = 'F', 'Female'
+        
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d/', default='images/profile2.png')
+    last_updated = models.DateTimeField(auto_now=True, blank=True)
+    gender = models.CharField(max_length=1, choices=Gender.choices, blank=True, null=True)
+    
+    def __str__(self):
+        return f'Profile of {self.user.username}'
